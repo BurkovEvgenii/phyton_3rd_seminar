1a26409fba5cf4a3bfb99946be9f8a2a3454b3b0
Burkov Evgenii <Burkovevgeniy@yansex.ru>
2022-10-28 16:23:35 +0300

    new

--- DZ_1_2.py
+++ DZ_1_2.py
@@ -6,11 +6,15 @@
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
 
-my_list = [2, 3, 5, 6]
-b = 0
-for i in range(len(my_list)):
-    if i == 0 or i%2 == 0:
-        b = my_list[i] * my_list[i-1]
-        print(b)
+my_list = [2, 3, 4, 5, 6]
+b = []
+if len(my_list)%2 == 0:
+    x = len(my_list)// 2
+else:
+    x = len(my_list)// 2 + 1    
+
+for i in range(x):
+     b.append(my_list[i] * my_list[-i-1])
+print(b)
     
 
