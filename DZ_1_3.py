c8ea15e82796c345d4cc62cf5810414056eb3d55
Burkov Evgenii <Burkovevgeniy@yansex.ru>
2022-10-28 16:52:17 +0300

    Finished 3rd task

--- DZ_1_3.py
+++ DZ_1_3.py
@@ -4,3 +4,35 @@
 # Пример:
 
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+my_list = [1.1, 1.2, 3.1, 5, 10.01]
+flo = []
+max = 0
+min = 0
+
+for i in range(len(my_list)):
+    flo.append(my_list[i]%1)
+print("Without int:", flo)
+
+for i in range(len (flo)):
+    flo=[round(v,3) for v in flo]
+print("Raund number:", flo)
+
+for i in range(len(flo)):
+    if flo[i] > flo[0]:
+        max = flo[i]
+print("Max number:", max)
+
+for i in range(len(flo)):
+    if flo[i] < flo[0] and flo[i] != 0:
+        min = flo[i]
+print("Min number:", min)
+print("Answer:", max - min)
+
+
+
+
+
+
+
+
